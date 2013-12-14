9ff2cacdade54f022c80fb8a2a6950560f62ff6a
Raj <rajkum@thoughtworks.com>
2013-12-14 11:15:14 +0530

    queue completed

---
+++ queue/queue_lib.h
@@ -0,0 +1,17 @@
+#include <stdbool.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
+typedef char String[256];
+typedef struct{
+	void **base;
+	int length;
+	int rear;
+} Queue;
+Queue* create(int length);
+bool enqueue(Queue *queue,void *element);
+void** getElement(Queue *queue,int index);
+void* dequeue(Queue *queue);
+bool isEmpty(Queue *queue);
+bool isFull(Queue *queue);
+void dispose(Queue *queue);
