be82ce03e04799e8d4dde530f1d9ad95ad73f964
Raj <rajkum@thoughtworks.com>
2013-12-16 11:37:25 +0530

    Added circular queue

---
+++ circular-queue/queue_lib.c
@@ -0,0 +1,34 @@
+#include "queue_lib.h"
+void** getElement(Queue *queue,int index){
+        return queue->base+(index);
+}
+Queue* create(int length){
+        Queue *queue = malloc(sizeof(Queue));
+        queue->base = malloc(sizeof(void *) * length);
+        queue->length = length;
+        queue->rear = -1;
+        queue->front = -1;
+        return queue;
+}
+void dispose(Queue *queue){
+        free(queue->base);
+        free(queue);
+}
+bool enqueue(Queue *queue, void *elementAddress){
+    void **address;
+    int rear;
+    rear = (queue->rear + 1)%queue->length;
+    if(rear == queue->front)
+            return false;
+    queue->rear = rear;
+    address = getElement(queue,queue->rear);
+    *address = elementAddress;
+    return true;
+}
+void* dequeue(Queue *queue){
+    int i;
+    if(queue->front == queue->rear)
+            return NULL;
+    queue->front = (queue->front + 1) % queue->length;
+    return *getElement(queue,queue->front);
+}
